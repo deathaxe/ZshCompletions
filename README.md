0d3d76836da16beb37468a973fde598d4da52ddf
deathaxe <deathaxe82@googlemail.com>
2024-11-09 18:08:53 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ZSH Completions
+
+Native ZSH auto completions for [Sublime Text](https://www.sublimetext.com).
+
+## Installation
+
+#### Package Control
+
+The easiest way to install is using Sublime's [Package Control](https://packagecontrol.io). It's listed as `ZSH Completions`.
+
+1. Open `Command Palette` using menu item `Tools → Command Palette...`
+2. Choose `Package Control: Install Package`
+3. Find `ZSH Completions` and hit `Enter`
---
+++ plugin.py
@@ -0,0 +1,220 @@
+import os
+import re
+import subprocess
+
+import sublime
+import sublime_plugin
+
+ZSH_CAPTURE_COMPLETION = R"""() {
+zmodload zsh/zpty || { echo 'error: missing module zsh/zpty' >&2; exit 1 }
+
+# spawn shell
+zpty z zsh -f -i
+
+# line buffer for pty output
+local line
+
+setopt rcquotes
+() {
+    zpty -w z source $1
+    repeat 4; do
+        zpty -r z line
+        [[ $line == ok* ]] && return
+    done
+    echo 'error initializing.' >&2
+    exit 2
+} =( <<< '
+# no prompt!
+PROMPT=
+
+# load completion system
+autoload compinit
+compinit -d ~/.zcompdump_capture
+
+# never run a command
+bindkey ''^M'' undefined
+bindkey ''^J'' undefined
+bindkey ''^I'' complete-word
+
+# send a line with null-byte at the end before and after completions are output
+null-line () {
+    echo -E - $''\0''
+}
+compprefuncs=( null-line )
+comppostfuncs=( null-line exit )
+
+# never group stuff!
+zstyle '':completion:*'' list-grouped false
+# don''t insert tab when attempting completion on empty line
+zstyle '':completion:*'' insert-tab false
+# no list separator, this saves some stripping later on
+zstyle '':completion:*'' list-separator ''''
+
+# load requirements for zparseopts
+zmodload zsh/zutil
+
+# override compadd (this our hook)
+compadd () {
+
+    # check if any of -O, -A or -D are given
+    if [[ ${@[1,(i)(-|--)]} == *-(O|A|D)\ * ]]; then
+        # if that is the case, just delegate and leave
+        builtin compadd "$@"
+        return $?
+    fi
+
+    # ok, this concerns us!
+    # echo -E - got this: "$@"
+
+    # be careful with namespacing here, we don''t want to mess with stuff that
+    # should be passed to compadd!
+    typeset -a __hits __dscr __tmp
+
+    # do we have a description parameter?
+    # note we don''t use zparseopts here because of combined option parameters
+    # with arguments like -default- confuse it.
+    if (( $@[(I)-d] )); then # kind of a hack, $+@[(r)-d] doesn''t work because of line noise overload
+        # next param after -d
+        __tmp=${@[$[${@[(i)-d]}+1]]}
+        # description can be given as an array parameter name, or inline () array
+        if [[ $__tmp == \(* ]]; then
+            eval "__dscr=$__tmp"
+        else
+            __dscr=( "${(@P)__tmp}" )
+        fi
+    fi
+
+    # capture completions by injecting -A parameter into the compadd call.
+    # this takes care of matching for us.
+    builtin compadd -A __hits -D __dscr "$@"
+
+    # set sane default options
+    setopt localoptions norcexpandparam extendedglob
+
+    # extract prefixes and suffixes from compadd call. we can''t do zsh''s cool
+    # -r remove-func magic, but it''s better than nothing.
+    typeset -A apre hpre hsuf asuf
+    zparseopts -E P:=apre p:=hpre S:=asuf s:=hsuf
+
+    # append / to directories? we are only emulating -f.
+    integer dirsuf=0
+    if [[ -z $hsuf && "${${@//-default-/}% -# *}" == *-[[:alnum:]]#f* ]]; then
+        dirsuf=1
+    fi
+
+    # just drop
+    [[ -n $__hits ]] || return
+
+    # this is the point where we have all matches in $__hits and all
+    # descriptions in $__dscr!
+
+    # display all matches
+    local dsuf dscr
+    for i in {1..$#__hits}; do
+
+        # add a dir suffix?
+        (( dirsuf )) && [[ -d $__hits[$i] ]] && dsuf=/ || dsuf=
+        # description to be displayed afterwards
+        (( $#__dscr >= $i )) && dscr=" -- ${${__dscr[$i]}##$__hits[$i] #}" || dscr=
+
+        echo -E - $IPREFIX$apre$hpre$__hits[$i]$dsuf$hsuf$asuf$dscr
+
+    done
+}
+
+# signal success!
+echo ok')
+
+zpty -w z "$*"$'\t'
+
+integer tog=0
+# read from the pty, and parse linewise
+while zpty -r z; do :; done | while IFS= read -r line; do
+    if [[ $line == *$'\0\r' ]]; then
+        (( tog++ )) && return 0 || continue
+    fi
+    # display between toggles
+    (( tog )) && echo -E - $line
+done
+
+return 2
+}"""
+"""
+ZSH capture completion
+
+https://github.com/Valodim/zsh-capture-completion
+
+Description
+
+Roughly, a pseudo-interactive zsh session is spawned using zpty, and a buffer
+string plus a tab character is sent so the complete-word widget is executed.
+To capture the hits, the compadd function is selectivly overridden in an
+inline-sourced file, capturing matches by injecting a parameter to the
+original compadd call and outputting matches to stdout.
+"""
+
+
+class ZshCompletionListener(sublime_plugin.EventListener):
+    enabled = True
+
+    def on_query_completions(self, view, prefix, locations):
+        if not self.enabled:
+            return None
+
+        pt = locations[0]
+        if not view.match_selector(pt, "source.shell - comment - string.quoted"):
+            return None
+
+        file_name = view.file_name()
+        cwd = os.path.dirname(file_name) if file_name else None
+
+        info = None
+        if os.name == 'nt':
+            info = subprocess.STARTUPINFO()
+            info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            info.wShowWindow = subprocess.SW_HIDE
+
+        try:
+            data = subprocess.check_output(
+                executable="zsh",
+                args=ZSH_CAPTURE_COMPLETION + " " + view.substr(view.line(pt)),
+                cwd=cwd,
+                shell=True,
+                startupinfo=info,
+                timeout=4.0
+            )
+            if not data:
+                return None
+
+        except subprocess.TimeoutExpired:
+            return None
+
+        except subprocess.SubprocessError:
+            self.enabled = False
+            print("ZSH Completion initialization failed, disabling completions!")
+            return None
+
+        except FileNotFoundError:
+            self.enabled = False
+            print("ZSH not found, disabling completions!")
+            return None
+
+        found = set()
+        completions = []
+
+        for line in str(data, encoding="utf-8").split("\r\n"):
+            parts = line.split(" -- ", 1)
+            word = parts[0]
+
+            if not word.startswith(prefix):
+                continue
+            if word in found:
+                continue
+            found.add(word)
+
+            if len(parts) > 1:
+                completions.append([word + "\t" + parts[1], word])
+            else:
+                completions.append(word)
+
+        return completions
